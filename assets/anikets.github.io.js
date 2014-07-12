4d5d74b27ab00eb5d6f4eec1716417c23edb4a3b
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2014-07-12 20:00:12 +0530

    Styling and markup changes.

--- assets/anikets.github.io.js
+++ assets/anikets.github.io.js
@@ -1,16 +1,3 @@
 $( function() {
-  Modernizr.addTest('backgroundclip',function() {
-
-    var div = document.createElement('div');
-
-    if ('backgroundClip' in div.style)
-      return true;
-
-    'Webkit Moz O ms Khtml'.replace(/([A-Za-z]*)/g,function(val) { 
-      if (val+'BackgroundClip' in div.style) return true;
-    });
-
-  });
-
   $( 'h1' ).fitText();
 });
